1f12b52bfcb2987c64d58dafbab98369c898d00d
BICHDAN <duonghoaibichdan.work@gmail.com>
2025-08-27 16:55:12 +0900

    learning about the branching statement in python

--- newlearning.py
+++ newlearning.py
@@ -514,3 +514,38 @@ print("" or 2)
 print(not "")
 
 
+#Branching Statement
+x = 3
+if x > 5:
+    print("x is bigger than 5")
+elif x <9:
+    print("x is smaller than 9")
+else:
+    print("x is nothing at all")
+    
+age = 17
+if age < 18: 
+    print("age is eligal")
+elif age >= 18:
+    print("age is not eligal at all")
+else:
+    print("can not be identified ")
+    
+x = 19
+if x > 10 and x < 20:
+    print("x is greater than 10 but smaller than 20")
+elif x <10 or x <= 0:
+    print("x is smaller than 10 and maybe = 0 ")
+else:
+    print("another circumstance")
+    
+Raining = False
+if not Raining:
+    print("it is not raining at all, Let's play outside")
+else :
+    print("let'S just stay inside the house")
+    
+#ternary operator
+age=20
+status = "大人" if age >= 18 else "子供"
+print(status)

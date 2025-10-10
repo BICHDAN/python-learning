f59ccebcca0e972e9bc5a441c1ed4538aa1984b9
BICHDAN <duonghoaibichdan.work@gmail.com>
2025-10-10 17:14:51 +0900

    learning about how to change attribute through method in OOP

--- newlearning2.py
+++ newlearning2.py
@@ -68,5 +68,46 @@ PowerRangerB = PowerRanger("Light", "Book", "Yellow")
 
 print(PowerRangerA.STT)
 print(PowerRangerB.STT)
+print(PowerRanger.so_thu_tu)
 
+class House:
+    def __init__(self, color, floors):
+        self.color = color
+        self.floors = floors
+
+house1 = House("red", 2)
+house2 = House("blue", 3)        
+
+# Use method to change attribute
+class Army:
+    def __init__(self, uniform, age, nickname ):
+        self.uniform = uniform
+        self.age = age
+        self.nickname = nickname
+    def Yelling(self):
+        print(f"yes sir!, {self.nickname} attended \nGot {self.uniform}, {self.age} years old")
+    
+    def birthday(self):
+        self.age += 1 
+        print(f"oh that you!, is today your birthday, congrat you are now {self.age}.")
+    
+    def reply(self):
+        print("Thank you sir\n")
         
+    def birthday2(self):
+        self.age += 10 
+        print(f"oh you lier, you're already {self.age} right.\n Don't be silly, you face is much older than me\n Seem like you can born me for sure! HaHa")
+    
+    def reply2(self):
+        print("Does that look obvious? Sir")
+        
+Army1 = Army("Swimming Uniform", 20, "BigMouth")
+Army2 = Army("Casual Uniform", 21, "GreenTomato")
+
+Army1.Yelling()
+Army1.birthday()
+Army1.reply()
+
+Army2.Yelling()
+Army2.birthday2()
+Army2.reply2()
